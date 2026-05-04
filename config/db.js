0a612abb5fb0237092262572b630eaff85957017
Rahul-college-use <rahulcollegeuse@gmail.com>
2026-05-04 14:57:28 +0530

    adddb1

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb+srv://GECJ:gecj123@gecj.o4nhqas.mongodb.net/?appName=GECj';
+const url = 'mongodb+srv://GECJ:gec123@gecj.o4nhqas.mongodb.net/?appName=GECj';
 const connectdb = mongoose.connect(url).then(()=>{
     console.log('Database connected successfully');
 })
